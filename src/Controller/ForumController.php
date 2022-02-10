5252386f4a3dd8da71431fb7a06c56a5b1f00da3
Céline Foucart <celinefoucart@yahoo.fr>
2022-02-10 11:45:48 +0100

    Page Category --end, Page Forum --start

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ForumRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ForumController extends AbstractController
 {
     private CategoryRepository $categoryRepository;
+    private ForumRepository $forumRepository;
 
-    public function __construct(CategoryRepository $categoryRepository)
+    public function __construct(CategoryRepository $categoryRepository, ForumRepository $forumRepository)
     {
         $this->categoryRepository = $categoryRepository;
+        $this->forumRepository = $forumRepository;
     }
 
     #[Route('/category/{slug}-{id}', name: 'category', requirements:['slug' => '[a-z\-]*'])]
@@ -21,13 +24,28 @@ class ForumController extends AbstractController
     {
         $category = $this->categoryRepository->find($id);
         if($category === null) {
-            throw $this->createNotFoundException('Catte catégorie n\'existe pas');
+            throw $this->createNotFoundException('Cette catégorie n\'existe pas');
         } elseif ($category->getSlug() !== $slug) {
             return $this->redirectToRoute('category', ['id' => $category->getId(), 'slug' => $category->getSlug()]);
         }
 
         return $this->render('forum/category.html.twig', [
             'category' => $category,
+        ]);
+    }
+
+    #[Route('/forum/{slug}-{id}', name: 'forum', requirements:['slug' => '[a-z\-]*'])]
+    public function forum(int $id, string $slug): Response
+    {
+        $forum = $this->forumRepository->find($id);
+        if($forum === null) {
+            throw $this->createNotFoundException('Ce forum n\'existe pas');
+        } elseif($forum->getSlug() !== $slug) {
+            return $this->redirectToRoute('forum', ['id' => $forum->getId(), 'slug' => $forum->getSlug()]);
+        }
+
+        return $this->render('forum/forum.html.twig', [
+            'forum' => $forum
         ]);
     }
 }
---
+++ tests/Controller/ForumControllerTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\ForumRepository;
+use App\Tests\FixtureTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ForumControllerTest extends WebTestCase
+{
+    use FixtureTrait;
+
+    public function testCategoryPage(): void
+    {
+        $client = static::createClient();
+        $this->makeFixture();
+        $client->request('GET', $this->getCategoryPath("accueil"));
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Accueil');
+    }
+
+    public function testCategoryPageWithNotFound(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/category/invalid-1123456789');
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+    public function testCategoryWithInvalidSlug(): void
+    {
+        $client = static::createClient();
+        $this->makeFixture();
+        $path = $this->getCategoryPath("accueil");
+        $invalidPath = preg_replace("/accueil/", 'acceuillll', $path);
+        $client->request('GET', $invalidPath);
+        $this->assertResponseRedirects($path);
+    }
+
+    public function testForumPage(): void
+    {
+        $client = static::createClient();
+        $this->makeFixture();
+        $client->request('GET', $this->getForumPath('presentation'));
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Présentation');
+    }
+
+    public function testForumPageWithInvalidSlug(): void
+    {
+        $client = static::createClient();
+        $this->makeFixture();
+        $validPath = $this->getForumPath('presentation');
+        $invalidPath = preg_replace("/presentation/", 'prez', $validPath);
+        $client->request('GET', $invalidPath);
+        $this->assertResponseRedirects($validPath);
+    }
+
+    public function testForumWithNoTopics(): void
+    {
+        $client = static::createClient();
+        $this->makeFixture();
+        $client->request('GET', $this->getForumPath('presentation'));
+        $this->assertSelectorTextContains('h3', 'Ce forum ne contient aucun sujet');
+    }
+
+    public function testForumWithTopics(): void
+    {
+        $client = static::createClient();
+        $this->makeFixture();
+        $client->request('GET', $this->getForumPath('presentation'));
+        $this->assertSelectorTextNotContains('h3', 'Ce forum ne contient aucun sujet');
+    }
+
+    public function testForumNotFound(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/forum/invalid-123456789');
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+    private function getCategoryPath(string $slug): string
+    {
+        $repo = static::getContainer()->get(CategoryRepository::class);
+        $category = $repo->findOneBy(['slug' => $slug]);
+        return '/category/' . $category->getSlug() . '-' . $category->getId();
+    }
+    
+    private function getForumPath(string $slug): string
+    {
+        $repo = static::getContainer()->get(ForumRepository::class);
+        $forum = $repo->findOneBy(['slug' => $slug]);
+        return '/forum/' . $forum->getSlug() . '-' . $forum->getId();
+    }
+
+}
